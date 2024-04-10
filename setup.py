ddbf318fd2a23077cb1cec86ceeae681e3fc84ed
simondrue <simondrue@gmail.com>
2024-04-10 08:40:08 +0200

    Remove pandas as dep

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     entry_points={"console_scripts": ["eldorado = eldorado.main:app"]},
     test_suite="tests",
     python_requires=">=3.10",
-    install_requires=["typer", "rich", "pandas", "pod5"],
+    install_requires=["typer", "rich", "pod5"],
     author="Simon Opstrup Drue",
     author_email="simondrue@gmail.com",
 )
